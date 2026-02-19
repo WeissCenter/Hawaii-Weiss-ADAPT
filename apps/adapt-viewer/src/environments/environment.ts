549db6a685b115b193a2ae7303a7c2deea9112a7
Aaron Dyke <54460524+AaronDyke@users.noreply.github.com>
2026-02-19 08:53:45 -0500

    temp viewer API_URL

--- apps/adapt-viewer/src/environments/environment.ts
+++ apps/adapt-viewer/src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
   contentRoot: 'assets/text/generic',
   contentFileName: 'viewer-content-text.json',
   appDomain: 'viewer.adapt.k12.hi.us',
-  API_URL: '',
+  API_URL: 'https://u0oav6271b.execute-api.us-east-1.amazonaws.com/weiss-sandbox/',
   logoPath: 'assets/shared/logos/generic',
   logoExtension: 'svg',
   copyrightText: 'AEM Corporation.',
