cc9f789f9f3a9327eccaffed637b4b9a52981a56
Aaron Dyke <54460524+AaronDyke@users.noreply.github.com>
2026-02-19 08:24:02 -0500

    temp fix for angular environments

---
+++ apps/adapt-admin/src/environments/environment.production.ts
@@ -0,0 +1,29 @@
+import { NgxLoggerLevel } from 'ngx-logger';
+
+export const environment = {
+  // NgxLoggerLevels: TRACE|DEBUG|INFO|LOG|WARN|ERROR|FATAL|OFF
+  logLevel: NgxLoggerLevel.OFF,
+  API_URL: '',
+  VAPID_KEY: '',
+  cognitoRegion: 'us-east-1',
+  cognitoDomainName: '',
+  s3PublicAssetsDomainName: '',
+  clientId: '',
+  contentRoot: 'assets/text',
+  contentFileName: 'admin-content-text.json',
+  appDomain: 'admin.adapt.k12.hi.us',
+  enforceLogin: true,
+  envLabel: 'Prod',
+  enforceRole: true,
+  callbackUrl: '',
+  Cognito: {
+    userPoolId: '',
+    userPoolClientId: '',
+  },
+  loginContent: 'assets/content-labels.json',
+  pagesContent: 'assets/text/admin-content-text.json',
+  organizationName: 'User Organization',
+  logoPath: 'assets/shared/logos/generic',
+  logoExtension: 'svg',
+  copyrightText: 'AEM Corporation.',
+};
---
+++ apps/adapt-viewer/src/environments/envrionment.production.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  envLabel: 'Prod',
+  contentRoot: 'assets/text/generic',
+  contentFileName: 'viewer-content-text.json',
+  appDomain: 'viewer.adapt.k12.hi.us',
+  API_URL: '',
+  logoPath: 'assets/shared/logos/generic',
+  logoExtension: 'svg',
+  copyrightText: 'AEM Corporation.',
+};
